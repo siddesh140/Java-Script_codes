3e910cac92c347f9786178d8a07b7a05a1edf881
Siddesh Yerawar <114944843+siddesh140@users.noreply.github.com>
2024-06-27 00:49:27 +0530

    Add files via upload

---
+++ video 57/script.js
@@ -0,0 +1,33 @@
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log("siddesh")
+// }
+// let b = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(b + i)
+// }
+// let object = {
+//     name: "siddesh",
+//     class: "123",
+//     "roll no": "7654",
+//     id: "456"
+// };
+// for (const element in object) {
+//     console.log(element)
+
+
+for (const char of "Mango") {
+    console.log(char)
+}
+let i = 2;
+while (i < 8) {
+    console.log(i)
+    i++;
+}
+// do {
+//     i = 10;
+//     console.log(i)
+//     i++;
+// } while (i < 1);
+
+
