e5368d570a4d095d0335f948fb207667d731d49c
Siddesh Yerawar <114944843+siddesh140@users.noreply.github.com>
2024-06-26 17:08:44 +0530

    Add files via upload

---
+++ video 56/practice_set.js
@@ -0,0 +1,13 @@
+let b = 1;
+for (let i = 0; i < 100; i++) {
+    console.log(b + i)
+}
+let object = {
+    name: "siddesh",
+    class: "123",
+    "roll no": "7654",
+    id: "456"
+};
+for (const element in object) {
+    console.log(element)
+}
---
+++ video 56/script.js
@@ -0,0 +1,21 @@
+let marks = 5;
+let grace = 2;
+marks += grace;
+console.log(marks);
+console.log(marks - grace);
+console.log(marks * grace);
+console.log(marks / grace);
+console.log(marks ** grace);
+// ** GIVES EXPONETIAL ANS
+console.log(marks % grace);
+// % GIVES REMAINDER
+{
+    if (marks + grace > 3)
+        console.log("you are passed");
+    else
+        console.log("failed")
+}
+let a = 4
+let b = 3
+if (a != b && b != a)
+    console.log("yes")
